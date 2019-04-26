e70b021d32c043a1d44aff446a39c67c210d6819
Sean <w_angxin@qq.com>
2019-04-26 14:47:31 +0800

    v2 test pplive

---
+++ pplivePush/pplivePush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pplivePush
+//
+//  Created by Sean on 2019/4/26.
+//  Copyright © 2019 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
